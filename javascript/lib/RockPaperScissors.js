df4489cf3abd60d66da10b970579b35256a8bad0
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-05-12 17:06:06 +0100

    spotted some errors, everything green now

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -9,28 +9,27 @@ Player.prototype.picks = function(pick) {
 function Game(player1, player2) {
 	this.player1 = player1;
 	this.player2 = player2;
-}; 
+};
 
  Game.prototype.PAIRS = {
- 								rock: {beats: "Scissors"},
- 								paper: {beats: "Rock"},
- 								scissors: {beats: "Paper"}
+ 								rock: {beats: "scissors"},
+ 								paper: {beats: "rock"},
+ 								scissors: {beats: "paper"}
  }
 
- Game.prototype.winner = function() {
- 	if(this._isSamePick()) return null;
+Game.prototype.winner = function() {
+	if(this._isSamePick()) return null;
+	if(this.PAIRS[this.player1.pick]['beats'] === this.player2.pick) {
+		return this.player1;
+	}
+	else {
+		return this.player2;
+	}
+}
 
- 	if(this.PAIRS[this.player1.pick]['beats']=== this.player2.pick) {
- 			return this.player1;
- 		}
- 		else {
- 			return this.player2;
- 		}
- }
-
- Game.prototype._isSamePick = function() {
+Game.prototype._isSamePick = function() {
  	return this.player1.pick === this.player2.pick;
-};
+}
 
 
 
