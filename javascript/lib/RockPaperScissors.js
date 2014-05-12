2afc63162ef0115246bf1bca82aea33299df4b1c
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-05-12 16:23:11 +0100

    created Player, Game, picks() and an hashash of beats

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,17 +1,46 @@
 function Player(name) {
-  // initialize
-  this.name = name;
+	this.name = name
 };
 
 Player.prototype.picks = function(pick) {
-  this.pick = pick;
+	this.pick = pick
 };
 
 function Game(player1, player2) {
-  this.player1 = player1;
-  this.player2 = player2;
+	this.player1 = player1;
+	this.player2 = player2;
+}; 
+
+ Game.prototype.PAIRS = {
+ 								rock: {beats: "Scissors"},
+ 								paper: {beats: "Rock"},
+ 								scissors: {beats: "Paper"}
+ }
+
+ Game.prototype.winner = function() {
+ 	if(this._isSamePick()) return null;
+
+ 	if(this.PAIRS[this.player1.pick]['beats']=== this.player2.pick) {
+ 			return this.player1;
+ 		}
+ 		else {
+ 			return this.player2;
+ 		}
+ }
+
+ Game.prototype._isSamePick = function() {
+ 	return this.player1.pick === this.player2.pick;
 };
 
-Game.prototype.winner = function() {
-  return this.player1;
-};
+
+
+
+
+
+
+
+
+
+
+
+
