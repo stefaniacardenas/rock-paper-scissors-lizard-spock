855fac54662226374cc2cc1c472f830483a4fe24
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-05-13 15:22:40 +0100

    refactored the test cases

--- javascript/spec/RockPaperScissorsSpec.js
+++ javascript/spec/RockPaperScissorsSpec.js
@@ -49,13 +49,12 @@ describe("Rock-Paper-Scissors-Lizard-Spock", function() {
 
     describe('paper', function() {
 
-      it('should beat rock', function() {
+      it('should beat spock', function() {
 
-        player1.picks('paper');
-        player2.picks('rock');
-        expect(game.winner()).toBe(player1);
-
-      });
+        player1.picks('spock');
+        player2.picks('paper');
+        expect(game.winner()).toBe(player2);
+      })
 
       it('should lose to scissors', function() {
 
@@ -65,46 +64,47 @@ describe("Rock-Paper-Scissors-Lizard-Spock", function() {
 
       });
 
-    });
+      it('should lose to lizard', function() {
 
-    describe('scissors', function() {
-
-      it('should beat paper', function() {
-
-        player1.picks('scissors');
-        player2.picks('paper');
-        expect(game.winner()).toBe(player1);
-
-      });
-
-      it('should lose to rock', function() {
-
-        player1.picks('scissors');
-        player2.picks('rock');
+        player1.picks('paper');
+        player2.picks('lizard');
         expect(game.winner()).toBe(player2);
 
       });
 
     });
 
-    // describe('spock', function() {
+    describe('scissors', function() {
 
-    //   it('should beat scissors', function() {
-        
-    //     player1.picks('spock');
-    //     player2.picks('scissors');
-    //     expect(game.winner()).toBe(player1);
+      it('should beat lizard', function() {
 
-    // });
+        player1.picks('scissors');
+        player2.picks('lizard');
+        expect(game.winner()).toBe(player1);
 
-    //   it('should beat rock', function() {
-        
-    //     player1.picks('rock');
-    //     player2.picks('spock');
-    //     expect(game.winner()).toBe(player2);
+      });
 
-    // });
+       it('should lose to spock', function() {
 
+        player1.picks('scissors');
+        player2.picks('spock');
+        expect(game.winner()).toBe(player2);
+
+      });
+
+    });
+
+    describe('spock', function() {
+
+           it('should lose to lizard', function() {
+
+            player1.picks('lizard');
+            player2.picks('spock');
+            expect(game.winner()).toBe(player1);
+
+          });
+
+    });
   });
 
   describe('draws', function() {
